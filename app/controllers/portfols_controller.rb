1a6b7d6cb098d13e0255be9904cb27dfb78ec20b
lalit527 <lalit.slg007@gmail.com>
2018-03-07 14:22:09 +0530

    Added create and new portfolio

--- app/controllers/portfols_controller.rb
+++ app/controllers/portfols_controller.rb
@@ -2,4 +2,26 @@ class PortfolsController < ApplicationController
   def index
     @portfolio_item = Portfol.all
   end
+
+  def new
+    @portfolio_item = Portfol.new
+  end
+
+  def create
+    @portfolio_item = Portfol.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfols_path, notice: 'Your portfolio is live now' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+    def portfolio_params
+      params.require(:portfol).permit(:title, :subtitle, :body)
+    end
+
 end
