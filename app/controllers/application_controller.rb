b4a13949447463788d98ec897ce2268ca46f336e
lalit527 <lalit.slg007@gmail.com>
2018-03-08 16:55:12 +0530

    Added link on the homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,8 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
-  
+  before_action :set_title
+  def set_title
+    @page_title = "My Portfolio"
+  end
 end
