c042d0835d6035693fafc93ae4d35ebc8f703dfa
lalit527 <lalit.slg007@gmail.com>
2018-03-09 15:26:05 +0530

    Updated Master layout styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def login_helper
     if current_user.is_a?(GuestUser)
-      (link_to "Login", new_user_session_path, method: :get) +
-      "<br>".html_safe +
-      (link_to "Signup", new_user_registration_path, method: :get)
+      (link_to "Login", new_user_session_path, method: :get , class: "nav-link") +
+      " ".html_safe +
+      (link_to "Signup", new_user_registration_path, method: :get, class: "nav-link")
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: "nav-link"
     end
   end
 
