9e6ff5dc79c894fcb5a9d8ecc0f97d30e95b9296
lalit527 <lalit.slg007@gmail.com>
2018-03-09 10:52:27 +0530

    Before installing personal gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = CopyrightViewTool::Renderer.copyright 'Lalit', 'All rights reserved'
+  end
+end
+
+module CopyrightViewTool
+  class Renderer
+    def self.copyright name, msg
+      "</p>&copy; #{Time.now.year} | <b>#{name}</b> #{msg}</p>".html_safe
+    end
+  end
 end
