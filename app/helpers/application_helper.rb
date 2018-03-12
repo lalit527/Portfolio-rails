bbf072d5baea311ff9b526a09409d977c13726df
lalit527 <lalit.slg007@gmail.com>
2018-03-12 19:56:51 +0530

    Merging navigation file to single link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper(style="nav-link")
     if current_user.is_a?(GuestUser)
-      (link_to "Login", new_user_session_path, method: :get , class: "nav-link") +
+      (link_to "Login", new_user_session_path, method: :get , class: style) +
       " ".html_safe +
-      (link_to "Signup", new_user_registration_path, method: :get, class: "nav-link")
+      (link_to "Signup", new_user_registration_path, method: :get, class: style)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete, class: "nav-link"
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
 
@@ -19,4 +19,44 @@ module ApplicationHelper
   def copyright_generator
     PortfolioCopyrightTool::Renderer.copyright 'Lalit', 'All rights reserved'
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfols_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
 end
