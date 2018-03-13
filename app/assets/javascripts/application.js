3c4693ecb105fd80fed5da9a62c93270e6d3f371
lalit527 <lalit.slg007@gmail.com>
2018-03-13 12:10:37 +0530

    Added jquery-ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //= require bootstrap-sprockets
+//= require html5.sortable
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
