6dde00af19a620bcf0a0d000bc6954c0a373958c
lalit527 <lalit.slg007@gmail.com>
2018-03-14 12:55:29 +0530

    Added initial image upload

--- app/controllers/portfols_controller.rb
+++ app/controllers/portfols_controller.rb
@@ -69,6 +69,8 @@ class PortfolsController < ApplicationController
       params.require(:portfol).permit(:title,
                                       :subtitle,
                                       :body,
+                                      :main_image,
+                                      :thumb_image,
                                       technologies_attributes: [:name])
     end
 
